05198fb2bc5c95a894f157b1f22d3a53327bc18f
reotsugua <renanaugusto.ras@gmail.com>
2024-05-06 21:19:14 -0300

    add lib phpmail

--- forms/contact.php
+++ forms/contact.php
@@ -2,30 +2,33 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../PHPMAIL/src/Exception.php';
-require '../PHPMAIL/src/PHPMailer.php';
-require '../PHPMAIL/src/SMTP.php';
+require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
 try {
+    // Configurações do servidor
     $mail->isSMTP();
-    $mail->Host = 'smtp.titan.email';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'noreply@rentstay.com.br'; // Assegure-se de que este é o e-mail correto para autenticação SMTP
-    $mail->Password = 'xUjabr(h'; // Certifique-se de que a senha está correta
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
+    $mail->Host       = 'smtp.titan.email';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'noreply@rentstay.com.br';
+    $mail->Password   = 'xUjabr(h'; 
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
 
-    $mail->setFrom('noreply@rentstay.com.br', 'Rent Stay');
-    $mail->addAddress('otsugua.renan@gmail.com'); // Substitua pelo destinatário correto
+    // Quem está enviando o e-mail
+    $mail->setFrom('noreply@rentstay.com.br', 'Rent Stay Formulario de Contato');
 
+    // Destinatário
+    $mail->addAddress('contato@rentstay.com.br', 'formulario de contato');
+
+    // Conteúdo
     $mail->isHTML(true);
     $mail->Subject = $_POST['subject'];
     $mail->Body    = 'Nome: ' . $_POST['name'] . '<br>Email: ' . $_POST['email'] . '<br>Mensagem: ' . $_POST['message'];
 
     $mail->send();
-    echo 'Message has been sent';
+    echo 'OK';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
